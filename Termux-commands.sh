30359f3a275a1a7686ea0b33592d2316b1239990
jusifake <102470125+KSSscriptt@users.noreply.github.com>
2022-03-30 12:44:52 +0600

    Create Termux-commands.sh

---
+++ Termux-commands.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade
+pkg install python
+pip install random
+pkg install git
+git clone https://github.com/https://github.com/KSSscriptt/password-generator
+cd password-generator
+python3 generation.py
